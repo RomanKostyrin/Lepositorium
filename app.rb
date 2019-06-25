df2c3d6f6fe33ec612212ae86f91dfdd36abe435
RomanKostyrin <enemy-iubip@mail.ru>
2019-06-25 09:23:00 +0300

    Add some features

--- app.rb
+++ app.rb
@@ -4,5 +4,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello!"			
+end
+
+get '/other' do
+erb :other
+end
+
+get '/ror' do
+erb :ror
+end
+
+get '/sinatra' do
+erb :sinatra
 end
